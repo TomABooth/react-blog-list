956bef255af1498fa268d47676ca1bdbad973155
Tom Booth <tombeezle@gmail.com>
2023-01-17 12:48:22 -0800

    blogs now showing

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchBlogs } from '../../../services/blogs.js';
+import BlogCard from '../../BlogCard/BlogCard.js';
 
 export default function Main() {
-  return <div>Main</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchBlogs();
+      setBlogs(resp);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+    </main>
+  );
 }
